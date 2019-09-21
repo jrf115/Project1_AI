09539215bafd940bb4f90b6a2400297a4281250e
WheatlyInc <jrf5001@yahoo.com>
2019-09-21 16:02:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project1_AI
+ This project is to help me become more familiar with Python so that I can get into AI more easily
---
+++ p1_0000.py
@@ -0,0 +1,20 @@
+###                                        ###
+# John Fahringer 2019 jrf115@zips.uakron.edu #
+# Project 1 - AI & Heurisitcs Analysis       #
+###                                        ###
+
+
+def myreverse:
+    something = 23
+
+
+def mirror:
+
+
+def flatten:
+
+
+def int_list:
+
+
+def invert_dict:
